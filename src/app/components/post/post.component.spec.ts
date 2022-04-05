30eef6fc6898eccc71dc6c41ec491be7ec07a172
leelanarasimha <leela.narsimha@gmail.com>
2022-04-05 23:12:16 +0530

    debug element angular testing

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { first } from 'rxjs';
 import { Post } from 'src/app/models/Post';
 import { PostComponent } from './post.component';
@@ -28,6 +29,17 @@ describe('Post Component', () => {
     const postElement: HTMLElement = fixture.nativeElement;
     const a = postElement.querySelector('a');
     expect(a?.textContent).toContain(post.title);
+  });
+
+  it('should render the post title in the anchor element using debug element', () => {
+    const post: Post = { id: 1, body: 'body 1', title: 'title 1' };
+    comp.post = post;
+    fixture.detectChanges();
+    const postDebugElement = fixture.debugElement;
+    const aElement: HTMLElement = postDebugElement.query(
+      By.css('a')
+    ).nativeElement;
+    expect(aElement.textContent).toContain(post.title);
   });
 
   it('should raise an event when the delete post is clicked', () => {
