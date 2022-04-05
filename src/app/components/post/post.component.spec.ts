e58be4102e24d538a483af3301fe87a1cb5f4e25
leelanarasimha <leela.narsimha@gmail.com>
2022-04-05 21:17:14 +0530

    Native element testBed

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { first } from 'rxjs';
 import { Post } from 'src/app/models/Post';
@@ -9,6 +10,7 @@ describe('Post Component', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [PostComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(PostComponent);
@@ -17,6 +19,15 @@ describe('Post Component', () => {
 
   it('should create post component using TestBed', () => {
     expect(comp).toBeDefined();
+  });
+
+  it('should render the post title in the anchor element', () => {
+    const post: Post = { id: 1, body: 'body 1', title: 'title 1' };
+    comp.post = post;
+    fixture.detectChanges();
+    const postElement: HTMLElement = fixture.nativeElement;
+    const a = postElement.querySelector('a');
+    expect(a?.textContent).toContain(post.title);
   });
 
   it('should raise an event when the delete post is clicked', () => {
