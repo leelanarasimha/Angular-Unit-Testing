f46e623faa0108483b1910bce003e87dbeedadd5
leelanarasimha <leela.narsimha@gmail.com>
2022-04-10 13:41:12 +0530

    http client spy object created

---
+++ src/app/services/Post/post.service.spec.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { PostService } from './post.service';
+
+describe('Post Service', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let postService: PostService;
+  let POSTS = [
+    {
+      id: 1,
+      body: 'body 1',
+      title: 'title 1',
+    },
+    {
+      id: 2,
+      body: 'body 2',
+      title: 'title 2',
+    },
+    {
+      id: 3,
+      body: 'body 3',
+      title: 'title 3',
+    },
+  ];
+  beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    postService = new PostService(httpClientSpy);
+  });
+
+  describe('getPosts()', () => {
+    it('should return expected posts when getposts is called', (done: DoneFn) => {
+      httpClientSpy.get.and.returnValue(of(POSTS));
+      postService.getPosts().subscribe({
+        next: (posts) => {
+          expect(posts).toEqual(POSTS);
+          done();
+        },
+        error: () => {
+          done.fail;
+        },
+      });
+      expect(httpClientSpy.get).toHaveBeenCalledTimes(1);
+    });
+  });
+});
