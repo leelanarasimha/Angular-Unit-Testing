33c968c420336686f9fe3123550ad97369a8255e
leelanarasimha <leela.narsimha@gmail.com>
2022-04-12 23:32:21 +0530

    Post Service http Client Testing

---
+++ src/app/services/Post/post-http.service.spec.ts
@@ -0,0 +1,51 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { PostService } from './post.service';
+
+describe('postService (HttpClientTestingModule)', () => {
+  let postService: PostService;
+  let httpTestingController: HttpTestingController;
+  let POSTS = [
+    {
+      id: 1,
+      body: 'body 1',
+      title: 'title 1',
+    },
+    {
+      id: 2,
+      body: 'body 2',
+      title: 'title 2',
+    },
+    {
+      id: 3,
+      body: 'body 3',
+      title: 'title 3',
+    },
+  ];
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [PostService],
+      imports: [HttpClientTestingModule],
+    });
+
+    postService = TestBed.inject(PostService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  describe('getPosts()', () => {
+    it('should return posts when getPosts() is called', (done: DoneFn) => {
+      postService.getPosts().subscribe((data) => {
+        expect(data).toEqual(POSTS);
+        done();
+      });
+      const request = httpTestingController.expectOne(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      request.flush(POSTS);
+      expect(request.request.method).toBe('GET');
+    });
+  });
+});
